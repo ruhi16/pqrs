b1aaccacb3c0de5bb19578856bef66ab20499f25
ruhi16 <hndas2016@gmail.com>
2018-02-24 01:10:13 +0530

    home

--- resources/views/Layouts/supports.blade.php
+++ resources/views/Layouts/supports.blade.php
@@ -5,13 +5,13 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    {{--  <link rel="stylesheet" type="text/css" href="{{url('bs337/css/bootstrap.min.css')}}" >  --}}
-    {{--  <script src="{{url('jQ/jquery.min.js')}}"></script>
-    <script src="{{url('bs337/js/bootstrap.min.js')}}" integrity=""></script>  --}}
+    <link rel="stylesheet" type="text/css" href="{{url('bs337/css/bootstrap.min.css')}}" >
+    <script src="{{url('jQ/jquery.min.js')}}"></script>
+    <script src="{{url('bs337/js/bootstrap.min.js')}}" integrity=""></script>
  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  {{--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  --}}
 
 {{--  latest cdn  --}}
 {{--  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
