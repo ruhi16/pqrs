c658574725be6338d40451fe1478295316109ac6
ruhi16 <hndas2016@gmail.com>
2018-03-14 18:44:54 +0530

    home

--- resources/views/clssecMarksRegister.blade.php
+++ resources/views/clssecMarksRegister.blade.php
@@ -33,23 +33,13 @@
                   <table class="table table-condensed table-bordered">
                     <thead>
                       <tr>
-                        <th>SL</th>
+                        <th>#</th>
                         <th>Subject</th>
                         @php                             
                             $typeTotal = 0;
                         @endphp
-                        @foreach($exms as $ex)
-                          {{--  @php 
-                            $ee = $extpclsbs->where('exam_id', $ex->id)->where('extype_id', $et->id)->first();                            
-                          @endphp
-                          <th>{{$ee->fm}}</th>  --}}
-                          {{--  @foreach($extpclsbs as $extpclsb)
-                            @if($extpclsb->exam_id == $ex->id && $extpclsb->extype_id == $et->id)
-                                <th>{{$ex->name}}</th>
-                                @php $typeTotal += $extpclsb->fm; @endphp
-                            @endif
-                          @endforeach  --}}
-                          <th>{{ $ex->name }}</th>
+                        @foreach($exms as $ex)                          
+                            <th>{{$ex->name}}</th>                         
                         @endforeach
                         <th>Total/{{$typeTotal}}</th>
                         <th>Grade</th>
@@ -59,40 +49,40 @@
                     @php $allSubTotal = 0; @endphp
                     @foreach($clsbs as $clsb)   {{-- for each subject --}}
                     @if($et->id == $clsb->subject->extype->id)
-                        <tr>
-                        <td>{{$clsb->subject->id}}</td>
-                        <td>{{$clsb->subject->name}}</td>
-                        @php $total = 0; @endphp
-                        @foreach($exms as $ex)
-                        <td class="text-right">
-                            {{--  @foreach($stdcr->marksentries as $record)
-                                @if($stdcr->id == $record->studentcr_id 
-                                    && $clsb->subject->id == $record->clssub->subject->id
-                                    && $ex->id == $record->exmtypclssub->exam->id )
-                                    @if($record->marks >= 0)
-                                        {{ $record->marks }}
-                                        @php $total = $total + $record->marks; @endphp
-                                    @else
-                                        AB
-                                    @endif
-                                @endif                            
-                            @endforeach  --}}
-                            ExEtSb<br>
-                            {{$ex->id}}-{{$et->id}}-{{$clsb->subject_id}}
+                      <tr>
+                      <td>{{$clsb->subject->id}}</td>
+                      <td>{{$clsb->subject->name}}</td>
+                      @php $total = 0; @endphp
+                      @foreach($exms as $ex)
+                      <td class="text-right">
+                        @foreach($stdcr->marksentries as $record)
                             
-                        </td>
+                            @if($extpclsbs->where('exam_id', $ex->id)
+                                          ->where('extype_id', $et->id)
+                                          ->where('subject_id', $clsb->subject_id)
+                                          ->first()->id == $record->exmtypclssub_id)
+                            
+                                {{ $record->marks!= -99 ?: 'AB' }}
+                                @php 
+                                    $total = $total +  ($record->marks!= -99 ?: 0 );
+                                @endphp
+                            @endif
+                            
                         @endforeach
-                        <th class="text-right">{{ $total }}</th>
-                        @php 
-                            $allSubTotal += $total;
                         
-                        @endphp
-                        <td>
-                        @php $grd = ($total)*100; @endphp {{--$typeTotal --}}
+                      </td>
+                      @endforeach
+                      <th class="text-right">{{ $total }}</th>
+                      @php 
+                        $allSubTotal += $total;
+                        
+                      @endphp
+                      <td>
+                        @php $grd = ($total)*100; @endphp {{-- /$typeTotal --}}
                         {{ Message($et->name,$grd) }}
-                        </td>
-                        </tr>
-                    @endif
+                      </td>
+                      </tr>
+                      @endif
                       {{--  @php $grtotal = $grdTotal = 0; @endphp  --}}
                       @endforeach
                       
