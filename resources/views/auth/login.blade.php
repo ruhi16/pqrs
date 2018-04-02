279956d0e4efeb59eb776a1506b6ff722e0c029f
ruhi16 <hndas2016@gmail.com>
2018-04-02 12:08:48 +0530

    home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-{{--  @extends('layouts.app')  --}}  --}}
+{{--  @extends('layouts.app')  --}}  
 {{--  @section('content')  --}}
 
 @extends('layouts.baselayout')
@@ -75,7 +75,10 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+
+{{--  @endsection  --}}
 
 
 
