8c59cf612b8e667ac7b41e88fdba5384eaf6f586
ruhi16 <hndas2016@gmail.com>
2018-11-20 07:58:43 +0530

    home

--- resources/views/resultcrs/clssecResultSheetv4PDF.blade.php
+++ resources/views/resultcrs/clssecResultSheetv4PDF.blade.php
@@ -99,7 +99,7 @@
 															<th class="text-center">{{ $exam->name }}</th>
 														@endif
 													@endforeach
-													<th class="text-center" rowspan='2'>Grand Total<br>200</th>
+													<th class="text-center" rowspan='3'>Grand Total<br>200</th>
 													@if($mode_count > 1)			{{-- if it is IX class only --}}
 														<th class="text-center" rowspan='3'>Average<br>100</th>
 													@endif
@@ -141,141 +141,8 @@
 													
 												</tr>
 											</thead>
-
-
 											<tbody>
-												@php												
-													$clssubs = $clssubs->sortBy(function($query){
-														return $query->combination_no < 0 ? -$query->combination_no : $query->combination_no;
-													});
-
-													// dd($clssubs);
-
-													$combaddl_subj_state = false;
-													$combaddl_subj_count_const = 0;													
-												@endphp
-
-												@foreach($clssubs as $clssub)
-													@if( $clssub->extype_id == $extype->id )
-														<tr>
-															<td style="vertical-align: middle;"  height="30">{{ $clssub->name }}</td>
-														
-														@php
-															$extpmdclsb = $extpmdclsbs->where('extype_id', $extype->id)
-																			->where('subject_id', $clssub->subject_id);
-															$extype_total_avg_marks = 0;
-														@endphp
-														@foreach($exams as $exam)															
-															@if( $mode_count > 1 )	{{-- for class IX --}}
-																@php $subj_total = 0; @endphp
-																@foreach($modes->sortByDesc('id') as $mode)
-																	@php
-																		$etmcs = $extpmdclsb->where('exam_id', $exam->id)
-																					->where('mode_id', $mode->id)->first();
-																		$etmcs_fm =$extpmdclsb->where('exam_id', $exam->id)
-																					->where('mode_id', $mode->id)->first()->fm;
-
-																		$obmrk = $marks->where('exmtypmodclssub_id', $etmcs->id)->first();
-																		$mark = $obmrk == NULL ? '' : ($obmrk->marks < 0 ? 'AB' : $obmrk->marks);
-																		$mark_num = $obmrk == NULL ? 0 : ($obmrk->marks < 0 ? 0 : $obmrk->marks);
-																	@endphp
-																	<td align="center" style="vertical-align: middle;">																
-																		{{ $mark }}
-																	</td>
-																	@php $subj_total += $mark_num; @endphp
-																@endforeach
-																<td align="center" style="vertical-align: middle;">
-																	<b>{{ $subj_total }}</b>
-																</td>
-															@else					{{-- for class V to VIII --}}
-																@php
-																	$etmcs = $extpmdclsb->where('exam_id', $exam->id)->first();
-																	$obtmark = $marks->where('exmtypmodclssub_id', $etmcs->id);
-
-																	$obtmark = $obtmark->first();
-																	$obtmark = $obtmark == NULL ? '' : ($obtmark->marks < 0 ? 'AB' : $obtmark->marks);
-
-																@endphp
-																<td  align="center" style="vertical-align: middle; font-size:18px;">
-																	{{ $obtmark }}
-																</td>
-															@endif
-														@endforeach
-
-														{{-- for subject Total, Average(only for IX), Grade--}}
-
-														@php
-															$totalObtMarks = $marks->whereIn('exmtypmodclssub_id', $extpmdclsb->pluck('id') )
-																		->where('marks', '>', 0)->sum('marks');
-															$totalFulMarks = $extpmdclsb->where('fm', '>', 0)->sum('fm');				
-															
-															$grade = getGrade($extype->id, $totalObtMarks, $totalFulMarks);
-															$percentage = round( ( $totalObtMarks * 100 ) / $totalFulMarks , 0); //optional
-
-															
-															if( $clssub->combination_no != 0 ){
-																//to find subject_ids of all commbined and additonal subjects
-																$curr_clssubs_comb      = $clssubs->where('combination_no', $clssub->combination_no);
-																$curr_clssubs_addl      = $clssubs->where('combination_no', -$clssub->combination_no);
-																$curr_clssubs = $curr_clssubs_comb->merge($curr_clssubs_addl);
-
-																$combaddl_subj_ids = $curr_clssubs->where('combination_no', '!=', 0)->pluck('id'); //fm er janya only combined, om er janya all comb+addl
-																$combaddl_subj_count = $curr_clssubs->where('combination_no', '!=', 0)->count('id');
-																
-																//to find extpmdclsb_ids of all combined & addl subjects to get obtained marks
-																$combaddl_etmcs_ids = $extpmdclsbs->where('extype_id', $extype->id)->whereIn('subject_id', $combaddl_subj_ids)->pluck('id');
-																if( $combaddl_subj_count > 0 && $combaddl_subj_state == false){
-																	$combaddl_subj_state = true;
-																	$combaddl_subj_count_const = $combaddl_subj_count;
-																}
-															}														
-
-														@endphp
-
-														<td  align="center" style="vertical-align: middle; font-size:18px;"><b>{{ $totalObtMarks }}</b></td>
-														
-														@if($mode_count > 1)	{{-- only for IX, Average --}}
-															<td  align="center" style="vertical-align: middle; font-size:18px;"><b>{{ $percentage }}</b></td>
-														@endif
-
-														@if ( $combaddl_subj_state == false )													
-															<td  align="center" style="vertical-align: middle; font-size:18px;">{{ $grade }}</td>												
-														@endif
-
-														
-
-														@if( $combaddl_subj_state == true && $combaddl_subj_count_const == $combaddl_subj_count )
-															@php
-																// obtain marks = total marks obtained in comb or addl subjects, if comb = 0, then goto addl
-																// fulll marks  = total marks either in comb or addl subjects
-																$comb_subj_ids = $clssubs->where('combination_no', $clssub->combination_no)->pluck('subject_id');															
-																$comb_subj_fm = $extpmdclsbs->whereIn('subject_id', $comb_subj_ids)->where('fm', '>', 0)->sum('fm');
-
-																$comb_etmcs_ids = $extpmdclsbs->whereIn('subject_id', $comb_subj_ids)->pluck('id');
-																$comb_total_ob_marks = $marks->whereIn('exmtypmodclssub_id', $comb_etmcs_ids )
-																							->where('marks', '>', 0)->sum('marks');
-
-																$percentage = round( ( $comb_total_ob_marks * 100 ) / $comb_subj_fm  , 0); //optional
-																$grade = getGrade($extype->id, $comb_total_ob_marks , $comb_subj_fm );
-																
-															@endphp
-															
-															<td rowspan="{{$combaddl_subj_count_const}}"  align="center" style="vertical-align: middle; font-size:18px;">
-																{{ $grade }}
-															</td>
-														@endif
-
-														@php $combaddl_subj_count_const > 0 ? $combaddl_subj_count_const-- : $combaddl_subj_count_const ; @endphp
-
-														@if( $combaddl_subj_state == true && $combaddl_subj_count_const == 0 )
-															@php $combaddl_subj_state = false; @endphp
-														@endif
-
-														
-
-														</tr>																							
-													@endif												
-												@endforeach											
+											
 											</tbody>
 										</table>										
 									</td>
@@ -292,34 +159,8 @@
 						@endphp     
 
 						@if( $isExist > 0 ) {{-- if any record exists for the specifix extype !!! --}}
-									<td>
-										
-										<b>Total Obtained Marks: </b>
-										@php
-											$subj_et_regular_ids = $clssubs->where('combination_no', 0)->pluck('subject_id');
-											// dd($subj_et_regular_ids);
-											$etmcs = $extpmdclsbs->where('extype_id', $extype->id)
-														->whereIn('subject_id', $subj_et_regular_ids)
-														->pluck('id');
-											$obtMarks = $marks->whereIn('exmtypmodclssub_id', $etmcs)
-															->where('studentcr_id', $studentcrs->first()->id)
-															->where('marks', '>', 0)
-															->sum('marks');
-
-											$subj_et_ids = $subjects->where('extype_id', $extype->id)->pluck('id');
-											$clssubs_reg = $clssubs->where('combination_no', '=', 0)
-															->whereIn('subject_id', $subj_et_ids)
-															->pluck('subject_id');
-											
-											$full_marks = $extpmdclsbs->whereIn('subject_id', $clssubs_reg)->sum('fm');
-											$obt_perc = round( (($obtMarks / $full_marks) * 100), 2 );
-										@endphp
-										{{ (int)($obtMarks/2) }} ({{ convert((int)$obtMarks/2) }}), ({{$obt_perc}}%), [Full Marks: {{$full_marks/2}}] 
-										
-										{{--  <b>Total No of 'Ds' Obtained: </b>  --}}
-									
-									
-									
+									<td>										
+										<b>Total Obtained Marks: </b>									
 									</td>
 									
 						@endif
@@ -327,7 +168,7 @@
 					
 					</tr>
 					<tr>
-						<td colspan="2">Result: </td>
+						<td colspan="2"><b>Result: </b></td>
 					</tr>
 				@endforeach
 			</tbody>
@@ -395,7 +236,7 @@
 			</tr>
 			</table>
 
-		<img src="{{ url('rubindicator/rubricindicator2.png') }}" class="img-rounded" width="100%">
+		{{--  <img src="{{ url('rubindicator/rubricindicator2.png') }}" class="img-rounded" width="100%">  --}}
 
 
 
