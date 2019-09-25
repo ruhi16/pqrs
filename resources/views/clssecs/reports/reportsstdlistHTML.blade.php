a47a1c3c49b38da173ece017404e47ffbe1a6057
ruhi16 <hndas16@gmail.com>
2019-09-25 23:35:57 +0530

    home

--- resources/views/clssecs/reports/reportsstdlistHTML.blade.php
+++ resources/views/clssecs/reports/reportsstdlistHTML.blade.php
@@ -25,45 +25,37 @@
 
 
     <body>
-        <center>
-        <h2>{{ $school->name }}</h2>
-        <h3>Studets Master Roll for ___________________________________, Session: {{ $session->name }}</h3>
-        <h4>Class: <b>{{ $clss->name }}, Section: {{ $section->name }}, Teacher: ______________________________, Date: ________</b></h4>
-        </center>
+        
+        <h2 align="center">{{ $school->name }}</h2>
+        <h3 align="center">Studets Master Roll for ___________________________________, Session: {{ $session->name }}</h3>
+        <h4 align="center">Class: <b>{{ $clss->name }}, Section: {{ $section->name }}, Teacher: ______________________________, Date: ______________</b></h4>
+        
         <table border="1" width="100%">
             <thead>
-            <tr>
-                <th>SL</th>
-                <th>Name</th>
-                <th>Roll</th>
-                {{-- @foreach($exms as $ex)
-                    <th>{{ $ex->name }}</th>
-                @endforeach --}}
-                {{--  @foreach($subjs as $subj)
-                    <th>{{ $subj->code }}</th>
-                @endforeach  --}}
-                <th>Signature</th>
-                <th>Remarks</th>
+            <tr height="200">
+                <th width="5%"><small>Roll</small></th>
+                <th width="20%">Name(<small>{{ $clss->name }}-{{ $section->name }})</small></th>
+                <th width="10%">DOB</th>
+                <th width="30%">Bank Details</th>
+                <th width="10%"><small>Adm Sl - Date</small></th>
+                <th width="25%"><small>Aadhaar/Caste</small></th>
+                                
             </tr>
             </thead>
             <tbody>
                 @php $i=1; @endphp
                 @foreach($stdList as $std)
                 <tr>
-                    <td>{{ $i }}</td>
-                    <td>{{ $std->studentdb->name }}</td>
-                    <td>{{ $i++ }}</td>
-                    {{-- @foreach($exms as $ex)
-                        <td></td>
-                    @endforeach --}}
-                    {{--  @foreach($subjs as $subj)
-                        <th></th>
-                    @endforeach  --}}
+                    <td height="60" align="center"><small>{{ $i++ }}</small></td>
+                    <td valign="top"><small>{{ $std->studentdb->name }}<BR/>FN:<BR/>V/P:</small></td>                
                     <td></td>
+                    <td><small>A/c No:<BR/>Bank/Br: <br/>IFSC:</small></td>
                     <td></td>
+                    <td valign="bottom"><hr><small>GEN/SC/ST/OBC-A/OBC-B</small></td>
                 </tr>
                 @endforeach
             </tbody>
+            
         </table>
 
     </body>
